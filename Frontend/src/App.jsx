d4e419222935bc91d3f2224c7b52c1938f94c1b1
Nayan <nayanbhusari02@gmail.com>
2026-03-12 12:29:51 +0530

    changes again deployement issue fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    document.title = "ReelNova";
+  }, []);
+
   return (
     <div>
       <Navbar />
